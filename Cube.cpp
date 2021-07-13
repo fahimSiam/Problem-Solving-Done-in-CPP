6dc57dd9c3bc1b36103e04eb88f1fa4c8e01d1df
fahimSiam <siamfahim02@gmail.com>
2021-07-13 16:24:29 +0600

    Create Cube.cpp

---
+++ Cube.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a;
+    b=0;
+    while(a>b){
+        int n,len;
+        cin>>n;
+        len=sqrt(n/6);
+        b++;
+    cout << len << endl;
+    }
+    return 0;
+}
