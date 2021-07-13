7241d55431c952642b1c3db631a86759603b79d0
fahimSiam <siamfahim02@gmail.com>
2021-07-13 15:55:19 +0600

    Create Mahmoud and Ehab and the even-odd game.cpp

---
+++ Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main(){
+    int a;
+    cin>>a;
+    if(a%2==0){
+        cout<<"Mahmoud"<<endl;
+    }
+    else{
+        cout<<"Ehab"<<endl;
+    }
+    return 0;
+}
